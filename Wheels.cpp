05ec016168ad1b23ee864d61f90039db00fdeb00
thergh <lukasz.ciswicki@gmail.com>
2024-04-24 10:33:29 +0200

    formatting

--- Wheels.cpp
+++ Wheels.cpp
@@ -5,27 +5,15 @@
 
 // defining arrows
 
-uint8_t arrowRight[8] = // glyph array??
-{
-    0b01000,
-    0b01100,
-    0b00110,
-    0b11111,
-    0b11111,
-    0b00110,
-    0b01100,
-    0b01000
-};
+uint8_t arrowRight[8] = {0b01000, 0b01100, 0b00110, 0b11111, 0b11111, 0b00110, 0b01100, 0b01000};
 
 
 #define SET_MOVEMENT(side,f,b) digitalWrite( side[0], f);\
                                digitalWrite( side[1], b)
 
-Wheels::Wheels() 
-{ }
+Wheels::Wheels(){ }
 
-void Wheels::attachRight(int pF, int pB, int pS)
-{
+void Wheels::attachRight(int pF, int pB, int pS){
     pinMode(pF, OUTPUT);
     pinMode(pB, OUTPUT);
     pinMode(pS, OUTPUT);
@@ -35,8 +23,7 @@ void Wheels::attachRight(int pF, int pB, int pS)
 }
 
 
-void Wheels::attachLeft(int pF, int pB, int pS)
-{
+void Wheels::attachLeft(int pF, int pB, int pS){
     pinMode(pF, OUTPUT);
     pinMode(pB, OUTPUT);
     pinMode(pS, OUTPUT);
@@ -45,72 +32,59 @@ void Wheels::attachLeft(int pF, int pB, int pS)
     this->pinsLeft[2] = pS;
 }
 
-void Wheels::setSpeedRight(uint8_t s)
-{
+void Wheels::setSpeedRight(uint8_t s){
     analogWrite(this->pinsRight[2], s);
 }
 
-void Wheels::setSpeedLeft(uint8_t s)
-{
+void Wheels::setSpeedLeft(uint8_t s){
     analogWrite(this->pinsLeft[2], s);
 }
 
-void Wheels::setSpeed(uint8_t s)
-{
+void Wheels::setSpeed(uint8_t s){
     setSpeedLeft(s);
     setSpeedRight(s);
 }
 
-void Wheels::attach(int pRF, int pRB, int pRS, int pLF, int pLB, int pLS)
-{
+void Wheels::attach(int pRF, int pRB, int pRS, int pLF, int pLB, int pLS){
     this->attachRight(pRF, pRB, pRS);
     this->attachLeft(pLF, pLB, pLS);
 }
 
-void Wheels::forwardLeft() 
-{
+void Wheels::forwardLeft(){
     SET_MOVEMENT(pinsLeft, HIGH, LOW);
 }
 
-void Wheels::forwardRight() 
-{
+void Wheels::forwardRight(){
     SET_MOVEMENT(pinsRight, HIGH, LOW);
 }
 
-void Wheels::backLeft()
-{
+void Wheels::backLeft(){
     SET_MOVEMENT(pinsLeft, LOW, HIGH);
 }
 
-void Wheels::backRight()
-{
+void Wheels::backRight(){
     SET_MOVEMENT(pinsRight, LOW, HIGH);
 }
 
-void Wheels::forward()
-{
+void Wheels::forward(){
     this->forwardLeft();
     this->forwardRight();
 }
 
-void Wheels::back()
-{
+void Wheels::back(){
     this->backLeft();
     this->backRight();
 }
 
-void Wheels::stopLeft()
-{
+void Wheels::stopLeft(){
     SET_MOVEMENT(pinsLeft, LOW, LOW);
 }
 
-void Wheels::stopRight()
-{
+void Wheels::stopRight(){
     SET_MOVEMENT(pinsRight, LOW, LOW);
 }
 
-void Wheels::stop()
-{
+void Wheels::stop(){
     this->stopLeft();
     this->stopRight();
 }
@@ -123,103 +97,103 @@ byte LCDAddress = 0x3F;
 
 long int intPeriod = 500000;
 
-void doBeep() {
+void doBeep(){
     digitalWrite(BEEPER, digitalRead(BEEPER) ^ 1);
   }
 
-void TimerUpdate(int period) {
-  Timer1.detachInterrupt();
-  Timer1.attachInterrupt(doBeep, period);
+void TimerUpdate(int period){
+    Timer1.detachInterrupt();
+    Timer1.attachInterrupt(doBeep, period);
 }
 
 void TimerStart(){
-  Timer1.detachInterrupt();
-  Timer1.attachInterrupt(doBeep, 400000);
+    Timer1.detachInterrupt();
+    Timer1.attachInterrupt(doBeep, 400000);
 }
 
 void TimerEnd(){
-  Timer1.detachInterrupt();
+    Timer1.detachInterrupt();
 }
 
 
 
 
 void Wheels::goForward(int cm){
-  LiquidCrystal_I2C lcd(LCDAddress, 16, 2);
+    LiquidCrystal_I2C lcd(LCDAddress, 16, 2);
 
-  lcd.setCursor(0, 0);
-
-  forward();
-  for(int i=0; i<cm; i++){
-    // printing 
-    // TODO: CHECK???
-    lcd.clear();
     lcd.setCursor(0, 0);
-    lcd.print(cm - i - 1);
-    
-    // printing distance left
-    lcd.setCursor(2, 0);
-    lcd.print("cm");
 
-    // printing speed
+    forward();
+    for(int i=0; i<cm; i++){
+        // printing 
+        // TODO: CHECK???
+        lcd.clear();
+        lcd.setCursor(0, 0);
+        lcd.print(cm - i - 1);
+
+        // printing distance left
+        lcd.setCursor(2, 0);
+        lcd.print("cm");
+
+        // printing speed
+        lcd.setCursor(0, 1);
+        lcd.print(255);
+        lcd.setCursor(12, 1);
+        lcd.print(255);
+
+        // applying the movement
+
+        delay(125);
+    }
+    stop();
+
+
+    lcd.clear();
     lcd.setCursor(0, 1);
-    lcd.print(255);
+    lcd.print(0);
     lcd.setCursor(12, 1);
-    lcd.print(255);
+    lcd.print(0);
 
-    // applying the movement
-    
-    delay(125);
-  }
-  stop();
-
-
-  lcd.clear();
-  lcd.setCursor(0, 1);
-  lcd.print(0);
-  lcd.setCursor(12, 1);
-  lcd.print(0);
-
-  // // end beeping
-  // TimerEnd();
+    // // end beeping
+    // TimerEnd();
 }
 
 void Wheels::goBack(int cm){
-  LiquidCrystal_I2C lcd(LCDAddress, 16, 2);
-  // // start beeping
-  // TimerStart();
-  
-  lcd.setCursor(0, 0);
-
-  back();
-  for(int i=0; i<cm; i++){
-    // printing 
-    // TODO: CHECK???
-    lcd.clear();
+    LiquidCrystal_I2C lcd(LCDAddress, 16, 2);
+    // // start beeping
+    // TimerStart();
+    
     lcd.setCursor(0, 0);
-    lcd.print(cm - i - 1);
-    
-    // printing distance left
-    lcd.setCursor(2, 0);
-    lcd.print("cm");
 
-    // printing speed
+    back();
+    for(int i=0; i<cm; i++){
+        // printing 
+        // TODO: CHECK???
+        lcd.clear();
+        lcd.setCursor(0, 0);
+        lcd.print(cm - i - 1);
+
+        // printing distance left
+        lcd.setCursor(2, 0);
+        lcd.print("cm");
+
+        // printing speed
+        lcd.setCursor(0, 1);
+        lcd.print(-255);
+        lcd.setCursor(12, 1);
+        lcd.print(-255);
+
+        // applying the movement
+
+        delay(125);
+    }
+    stop();
+
+    lcd.clear();
     lcd.setCursor(0, 1);
-    lcd.print(-255);
+    lcd.print(0);
     lcd.setCursor(12, 1);
-    lcd.print(-255);
-
-    // applying the movement
-    
-    delay(125);
-  }
-  stop();
-
-  lcd.clear();
-  lcd.setCursor(0, 1);
-  lcd.print(0);
-  lcd.setCursor(12, 1);
-  lcd.print(0);
+    lcd.print(0);
 }
 
 
