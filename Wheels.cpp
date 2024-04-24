2212cbfbfd4becc41822a38b629d0e12a1183f53
thergh <lukasz.ciswicki@gmail.com>
2024-04-24 09:46:14 +0200

    initial

---
+++ Wheels.cpp
@@ -0,0 +1,225 @@
+#include <Arduino.h>
+#include <LiquidCrystal_I2C.h>
+#include "Wheels.h"
+#include "TimerOne.h"
+
+// defining arrows
+
+uint8_t arrowRight[8] = // glyph array??
+{
+    0b01000,
+    0b01100,
+    0b00110,
+    0b11111,
+    0b11111,
+    0b00110,
+    0b01100,
+    0b01000
+};
+
+
+#define SET_MOVEMENT(side,f,b) digitalWrite( side[0], f);\
+                               digitalWrite( side[1], b)
+
+Wheels::Wheels() 
+{ }
+
+void Wheels::attachRight(int pF, int pB, int pS)
+{
+    pinMode(pF, OUTPUT);
+    pinMode(pB, OUTPUT);
+    pinMode(pS, OUTPUT);
+    this->pinsRight[0] = pF;
+    this->pinsRight[1] = pB;
+    this->pinsRight[2] = pS;
+}
+
+
+void Wheels::attachLeft(int pF, int pB, int pS)
+{
+    pinMode(pF, OUTPUT);
+    pinMode(pB, OUTPUT);
+    pinMode(pS, OUTPUT);
+    this->pinsLeft[0] = pF;
+    this->pinsLeft[1] = pB;
+    this->pinsLeft[2] = pS;
+}
+
+void Wheels::setSpeedRight(uint8_t s)
+{
+    analogWrite(this->pinsRight[2], s);
+}
+
+void Wheels::setSpeedLeft(uint8_t s)
+{
+    analogWrite(this->pinsLeft[2], s);
+}
+
+void Wheels::setSpeed(uint8_t s)
+{
+    setSpeedLeft(s);
+    setSpeedRight(s);
+}
+
+void Wheels::attach(int pRF, int pRB, int pRS, int pLF, int pLB, int pLS)
+{
+    this->attachRight(pRF, pRB, pRS);
+    this->attachLeft(pLF, pLB, pLS);
+}
+
+void Wheels::forwardLeft() 
+{
+    SET_MOVEMENT(pinsLeft, HIGH, LOW);
+}
+
+void Wheels::forwardRight() 
+{
+    SET_MOVEMENT(pinsRight, HIGH, LOW);
+}
+
+void Wheels::backLeft()
+{
+    SET_MOVEMENT(pinsLeft, LOW, HIGH);
+}
+
+void Wheels::backRight()
+{
+    SET_MOVEMENT(pinsRight, LOW, HIGH);
+}
+
+void Wheels::forward()
+{
+    this->forwardLeft();
+    this->forwardRight();
+}
+
+void Wheels::back()
+{
+    this->backLeft();
+    this->backRight();
+}
+
+void Wheels::stopLeft()
+{
+    SET_MOVEMENT(pinsLeft, LOW, LOW);
+}
+
+void Wheels::stopRight()
+{
+    SET_MOVEMENT(pinsRight, LOW, LOW);
+}
+
+void Wheels::stop()
+{
+    this->stopLeft();
+    this->stopRight();
+}
+
+
+byte LCDAddress = 0x3F;
+
+// TIMER --- INTERRUPT
+// aktualizuje Timer1 aktualną wartością intPeriod
+
+long int intPeriod = 500000;
+
+void doBeep() {
+    digitalWrite(BEEPER, digitalRead(BEEPER) ^ 1);
+  }
+
+void TimerUpdate(int period) {
+  Timer1.detachInterrupt();
+  Timer1.attachInterrupt(doBeep, period);
+}
+
+void TimerStart(){
+  Timer1.detachInterrupt();
+  Timer1.attachInterrupt(doBeep, 400000);
+}
+
+void TimerEnd(){
+  Timer1.detachInterrupt();
+}
+
+
+
+
+void Wheels::goForward(int cm){
+  LiquidCrystal_I2C lcd(LCDAddress, 16, 2);
+
+  lcd.setCursor(0, 0);
+
+  forward();
+  for(int i=0; i<cm; i++){
+    // printing 
+    // TODO: CHECK???
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print(cm - i - 1);
+    
+    // printing distance left
+    lcd.setCursor(2, 0);
+    lcd.print("cm");
+
+    // printing speed
+    lcd.setCursor(0, 1);
+    lcd.print(255);
+    lcd.setCursor(12, 1);
+    lcd.print(255);
+
+    // applying the movement
+    
+    delay(125);
+  }
+  stop();
+
+
+  lcd.clear();
+  lcd.setCursor(0, 1);
+  lcd.print(0);
+  lcd.setCursor(12, 1);
+  lcd.print(0);
+
+  // // end beeping
+  // TimerEnd();
+}
+
+void Wheels::goBack(int cm){
+  LiquidCrystal_I2C lcd(LCDAddress, 16, 2);
+  // // start beeping
+  // TimerStart();
+  
+  lcd.setCursor(0, 0);
+
+  back();
+  for(int i=0; i<cm; i++){
+    // printing 
+    // TODO: CHECK???
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print(cm - i - 1);
+    
+    // printing distance left
+    lcd.setCursor(2, 0);
+    lcd.print("cm");
+
+    // printing speed
+    lcd.setCursor(0, 1);
+    lcd.print(-255);
+    lcd.setCursor(12, 1);
+    lcd.print(-255);
+
+    // applying the movement
+    
+    delay(125);
+  }
+  stop();
+
+  lcd.clear();
+  lcd.setCursor(0, 1);
+  lcd.print(0);
+  lcd.setCursor(12, 1);
+  lcd.print(0);
+}
+
+
---
+++ auto.ino
@@ -0,0 +1,186 @@
+#include "Wheels.h"
+#include "TimerOne.h"
+#include "IRremote.hpp"
+#include "PinChangeInterrupt.h"
+
+// #define BLINK_PIN 
+Wheels w;
+volatile char cmd;
+
+
+// obsługa pilota
+// kody pilota:
+// 1: 69  2: 70  3: 71   4: 68   5: 64   6: 67   7: 7  8: 21   9: 9  0: 22   *: 25   #: 13   up: 24  left: 8   right: 90   down: 82  ok: 28 
+constexpr uint8_t RECV_PIN {2};
+enum State{OFF, ON} state;
+
+const uint16_t s1 = 0x3F;  // Taste 5
+
+
+void setup(){
+  w.attach(12, 4, 3, 8, 7, 5);
+  pinMode(3, OUTPUT); // L power
+  pinMode(12, OUTPUT); // LT
+  pinMode(4, OUTPUT); // LP
+  pinMode(7, OUTPUT);  // PP
+  pinMode(8, OUTPUT);  // PT
+  pinMode(5, OUTPUT);  // P power
+
+  // set speed
+  analogWrite(3, 255);
+  analogWrite(5, 255);
+
+  // setup serial
+  Serial.begin(9600);
+  Serial.println("Forward: WAD");
+  Serial.println("Back: ZXC");
+  Serial.println("Stop: S");
+
+  Serial.print("Initiated pins.");
+
+  // setup timer1
+  // pinMode(LED_BUILTIN, OUTPUT);
+  // Timer1.initialize(1); // Fire An Interrupt Every 10ms
+  // // Timer1.attachInterrupt(blinkInterrupt);
+  // // Timer1.attachInterrupt(printInterrupt);
+  // // Timer1.attachInterrupt(signalInterrupt);
+
+  // setup
+
+  // setup radio receiver
+  IrReceiver.begin(RECV_PIN);
+  Serial.print(F("Ready to receive IR signals at pin "));
+  Serial.println(RECV_PIN);
+  // enter_password();
+}
+
+
+void loop(){
+  int time_current = millis();
+  uint16_t signal = irReceive();
+  perform_action(signal);
+
+  switch(signal){
+    case 24:
+      w.forward();
+      // w.goForward(5);
+      Serial.print("Signal: ");
+      Serial.print(signal);
+      Serial.print(", going forward\n");
+      break;
+    case 82:
+      // w.goBack(5);
+      w.back();
+      Serial.print("Signal: ");
+      Serial.print(signal);
+      Serial.print(", going forward\n");
+      break;
+  }
+
+  // while(Serial.available()){
+
+  //   cmd = Serial.read();
+  //   switch(cmd){
+  //     case 'w': w.forward(); break;
+  //     case 'x': w.back(); break;
+  //     case 'a': w.forwardLeft(); break;
+  //     case 'd': w.forwardRight(); break;
+  //     case 'z': w.backLeft(); break;
+  //     case 'c': w.backRight(); break;
+  //     case 's': w.stop(); break;
+  //     case '1': w.setSpeedLeft(75); break;
+  //     case '2': w.setSpeedLeft(200); break;
+  //     case '9': w.setSpeedRight(75); break;
+  //     case '0': w.setSpeedRight(200); break;
+  //     case '5': w.setSpeed(100); break;
+  //     case 'f': w.goForward(5); break;
+  //     case 'b': w.goBack(5); break;
+  //   }
+  // }
+}
+
+
+void signalInterrupt(void){
+  uint16_t signal = irReceive();
+  perform_action(signal);
+  Serial.print(signal);
+  digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
+}
+
+
+void blinkInterrupt(void){
+  digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
+  // Serial.print("Interrupted\n");
+}
+
+
+void printInterrupt(void){
+  Serial.print("interrupt\n");
+}
+
+
+// Receives signal and prints it to console
+uint16_t irReceive() {
+  uint16_t received{0};
+  
+  if (IrReceiver.decode()) {
+    IrReceiver.printIRResultShort(&Serial);
+    if (IrReceiver.decodedIRData.protocol == UNKNOWN) {
+      // We have an unknown protocol here, print more info
+      IrReceiver.printIRResultRawFormatted(&Serial, true);
+    }
+    if (IrReceiver.decodedIRData.protocol == NEC) {
+      received = IrReceiver.decodedIRData.command;
+      Serial.print("Command: 0x");
+      Serial.println(received, HEX);
+    }
+    IrReceiver.resume();
+  }
+
+  if(received != 0){
+    Serial.print("Received signal: ");
+    Serial.println(received);
+  }
+  
+  return received;
+}
+
+
+// Waits until user provides correct signal
+int enter_password(){
+  Serial.print("Waiting for a password\n");
+  int password = 111;
+  int entered = 000;
+  int signal = 0;
+  while(entered != 69){
+    entered = 000;
+    delay(100);
+    signal = irReceive();
+    entered = signal;
+  }
+  Serial.println("Entered a proper password");
+}
+
+
+// Performs action depending on signal
+void perform_action(uint16_t signal){
+  switch(signal){
+    case 24:
+      w.goForward(5);
+      Serial.print("Signal: ");
+      Serial.print(signal);
+      Serial.print(", going forward\n");
+      break;
+    case 82:
+      w.goBack(5);
+      Serial.print("Signal: ");
+      Serial.print(signal);
+      Serial.print(", going forward\n");
+      break;
+    // default:
+    //   Serial.print("Signal: ");
+    //   Serial.print(signal);
+    //   Serial.print(" not usable\n");
+  }
+}
+
